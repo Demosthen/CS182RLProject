fix some bugs with advantage calculation

Testing.py
@@ -6,8 +6,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 from ppotrainer import *
 from pponetwork import * 
-env = ProcgenGym3Env(num=1, env_name="fruitbot", render_mode="rgb_array")
-env = gym3.ViewerWrapper(env, info_key="rgb")
+import wandb
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+num_actors = 64
+env = ProcgenGym3Env(num=num_actors, env_name="fruitbot", render_mode="rgb_array", distribution_mode="easy")
+#env = gym3.ViewerWrapper(env, info_key="rgb")
 step = 0
 # TODO: decrease action space to just 3 
 # [
@@ -34,21 +40,40 @@ step = 0
 #     print(f"step {step} reward {rew} first {first}, action {act}")
 #     step += 1
 action_map = [1, 4, 7, 9]
-network_params = {'input_dims': (64, 64, 9),
+concat_mode = False
+impala_params = {"in_channels": 3,
+                 "depths": [16, 32, 32],
+                 "out_dim": 4}
+cnn_params = {'input_dims': (64, 64, 9) if concat_mode else (64, 64, 3),
                     'num_actions': 4,
-                    'conv_layer_sizes': [32, 64, 64],
-                    'fc_layer_sizes': [512],
-                    'strides': [4, 2, 1],
+                    'conv_layer_sizes': [8, 16],
+                    'fc_layer_sizes': [128],
+                    'strides': [4, 2],
+                    'filter_sizes': [8, 4]
                     }
-trainer = PPOTrainer(num_iters = 1000,
-                     num_actors = 1,
-                     num_timesteps = 16, 
-                     discount_factor = 0.99,
+use_impala = True
+if use_impala:
+    network_params = impala_params
+else:
+    network_params = cnn_params
+wandb.init(project="cs182rlproject")
+trainer = PPOTrainer(num_iters = 5000,
+                     num_actors = num_actors,
+                     num_timesteps = 256, 
+                     discount_factor = 0.999,
                      epsilon = 0.2,
-                     c1 = 0.01,
-                     c2 = 0.1,
+                     c1 = 0.1,
+                     c2 = 0.01,
                      optimizer = torch.optim.Adam,
-                     lr= 3e-4,
+                     lr= 5e-4,
                      action_map=action_map,
+                     lambd=0.95,
+                     num_epochs=3,
+                     batch_size = 256*num_actors // 8,
+                     concat_mode=concat_mode,
+                     device=device,
+                     use_impala=use_impala,
                      ppo_network_args = network_params)
+
+#print(trainer._compute_advantage_single_actor(torch.ones([10]), torch.ones([10]), [2, 5]))
 trainer.train(env)

pponetwork.py
@@ -6,16 +6,48 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-class ConvBlock(nn.Module):
-    def __init__(self, in_channels, filter_size, stride=1):
+class ResidBlock(nn.Module):
+    def __init__(self, in_channels):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, filter_size, stride=stride, kernel_size=(3, 3), padding=1)
+        self.conv0 = nn.Conv2d(in_channels, in_channels, 3, padding=1)
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(in_channels, in_channels, 3, padding=1)
+    
+    def forward(self, input):
+        out = self.relu(input)
+        out = self.conv0(out)
+        out = self.relu(out)
+        out = self.conv1(out)
+        return out + input
+
+class ConvSequence(nn.Module):
+    def __init__(self, in_channels, depth):
+        super().__init__()
+        self.conv0 = nn.Conv2d(in_channels, depth, 3, padding=1)
+        self.max0 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.res0 = ResidBlock(depth)
+        self.res1 = ResidBlock(depth)
+    
+    def forward(self, input):
+        out = self.conv0(input)
+        out = self.max0(out)
+        out = self.res0(out)
+        out = self.res1(out)
+        return out
+        
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, filter_size, kernel_size, stride=1):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, filter_size, stride=stride, kernel_size=(kernel_size, kernel_size), padding=kernel_size // 2)
         self.batchnorm = nn.BatchNorm2d(filter_size)
+        with torch.no_grad():
+            self.conv.weight *= torch.sqrt(torch.tensor(2))
         self.relu = nn.ReLU()
 
     def forward(self, input):
         x = self.conv(input)
-        x = self.batchnorm(x)
+        #x = self.batchnorm(x)
         x = self.relu(x)
         return x
 
@@ -24,6 +56,8 @@ class FCBlock(nn.Module):
         super().__init__()
         print("hi", out_size, in_size)
         self.fc = nn.Linear(in_size, out_size)
+        with torch.no_grad():
+            self.fc.weight *= torch.sqrt(torch.tensor(2))
         self.batchnorm = nn.BatchNorm1d(out_size)
         self.relu = nn.ReLU()
 
@@ -34,25 +68,27 @@ class FCBlock(nn.Module):
         return x
 
 class PPONetwork(nn.Module):
-    def __init__(self, input_dims, num_actions, conv_layer_sizes, fc_layer_sizes, strides): 
+    def __init__(self, input_dims, num_actions, conv_layer_sizes, fc_layer_sizes, strides, filter_sizes): 
         super().__init__()
         self.input_dims = input_dims
         self.width, self.height, self.num_channels = input_dims #64 x 64 x 9
         self.num_actions = num_actions
         self.output_dims = num_actions + 1
         self.convs = []
-        self.convs.append(ConvBlock(self.num_channels, conv_layer_sizes[0], strides[0]))
+        self.filter_sizes = filter_sizes
+        self.convs.append(ConvBlock(self.num_channels, filter_sizes[0], conv_layer_sizes[0], strides[0]))
         for i in range(1, len(conv_layer_sizes)):
-            self.convs.append(ConvBlock(conv_layer_sizes[i-1], conv_layer_sizes[i], strides[i]))
+            self.convs.append(ConvBlock(conv_layer_sizes[i-1], filter_sizes[i], conv_layer_sizes[i], strides[i]))
         for i in range(len(self.convs)):
             self._modules["conv" + str(i)] = self.convs[i]
         
         power = np.prod([int(d) for d in strides])
-        in_size = int(conv_layer_sizes[-1] * self.width * self.height / (power**2))
+        #in_size = int(conv_layer_sizes[-1] * self.width * self.height / (power**2))
+        in_size = 324
         self.fcs = [nn.Linear(in_size, fc_layer_sizes[0])]
         for i in range(1, len(fc_layer_sizes)):
             self.fcs.append(FCBlock(fc_layer_sizes[i-1], fc_layer_sizes[i]))
-        self.fcs.append(FCBlock(fc_layer_sizes[-1], self.output_dims))
+        self.fcs.append(nn.Linear(fc_layer_sizes[-1], self.output_dims))
         for i in range(len(self.fcs)):
             self._modules["fc" + str(i)] = self.fcs[i]
         
@@ -71,5 +107,45 @@ class PPONetwork(nn.Module):
         for i in range(len(self.fcs)):
             x = self.fcs[i](x)
         return x
-            
+
+def compute_size(width, filter_size, stride, padding):
+    return (width - filter_size + 2 * padding) / stride + 1
+
+class IMPALA_CNN(nn.Module):
+    def __init__(self, in_channels, depths, out_dim):
+        super().__init__()
+        self.convs = [ConvSequence(in_channels, depths[0])]
+        
+        for i in range(1, len(depths)):
+            self.convs.append(ConvSequence(depths[i-1], depths[i]))
+        for i in range(len(self.convs)):
+            self._modules["conv" + str(i)] = self.convs[i]
+        size = 64 // (2 ** len(depths))
+        size = size ** 2
+        size = size * depths[-1]
+        self.relu = nn.ReLU()
+        self.fc0 = nn.Linear(size, 256)
+        self.fc1 = nn.Linear(256, out_dim)
+        self.val_fc0 = nn.Linear(size, 256)
+        self.val_fc1 = nn.Linear(256, 1)
+        self.out_dim = out_dim
     
+    def forward(self, input):
+        out = input
+        for i in range(len(self.convs)):
+            out = self.convs[i](out)
+        
+        out = torch.flatten(out, start_dim=1)
+        out = self.relu(out)
+        val_out = self.val_fc0(out)
+        val_out = self.relu(val_out)
+        val_out = self.val_fc1(val_out)
+        out = self.fc0(out)
+        out = self.relu(out)
+        out = self.fc1(out)
+        output = torch.cat([out, val_out], dim=-1)
+        return output
+
+        
+    
+

ppotrainer.py
@@ -2,16 +2,35 @@ import torch
 from torch import tensor
 import torch.nn as nn
 import torch.nn.functional as F
-from pponetwork import PPONetwork
+from pponetwork import IMPALA_CNN, PPONetwork
 import gym3
 from procgen import ProcgenGym3Env
 import numpy as np
 import torch.optim as optim
+import wandb
+from torch.distributions import Categorical
+
+def explained_variance(ypred,y):
+    """
+    Computes fraction of variance that ypred explains about y.
+    Returns 1 - Var[y-ypred] / Var[y]
+    interpretation:
+        ev=0  =>  might as well have predicted zero
+        ev=1  =>  perfect prediction
+        ev<0  =>  worse than just predicting zero
+    """
+    vary = torch.var(y)
+    return np.nan if vary==0 else 1 - torch.var(y-ypred)/vary
+
 class PPOTrainer():
-    def __init__(self, num_iters : int, num_actors : int, num_timesteps : int, discount_factor : float,
-                 epsilon : float, c1 : float, c2 : float, optimizer : optim.Optimizer, lr : float,
-                 action_map : list, ppo_network_args : dict):
-        self.pponetwork = PPONetwork(**ppo_network_args)
+    def __init__(self, num_iters : int, num_actors : int, num_timesteps : int, num_epochs : int, discount_factor : float,
+                 batch_size : int, epsilon : float, c1 : float, c2 : float, optimizer : optim.Optimizer, lr : float, 
+                 lambd : float, action_map : list, ppo_network_args : dict, device = "cpu", concat_mode=False, use_impala=False):
+        if use_impala:
+            self.pponetwork = IMPALA_CNN(**ppo_network_args).to(device=device)
+        else:
+            self.pponetwork = PPONetwork(**ppo_network_args).to(device=device)
+        wandb.watch(self.pponetwork)
         self.num_iters = num_iters
         self.num_actors = num_actors
         self.num_timesteps = num_timesteps
@@ -21,11 +40,16 @@ class PPOTrainer():
         self.c2 = c2
         self.action_map = action_map
         self.optimizer = optimizer(self.pponetwork.parameters(), lr = lr)
+        self.device = device
+        self.lambd = lambd
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.concat_mode = concat_mode
 
     def _compute_advantage_single_actor(self, values : tensor, rewards : tensor, firsts : list):
         """Computes advantage of a single actor"""
         T = len(rewards)
-        coeffs = self.discount_factor ** torch.arange(0, T-1)
+        coeffs = (self.discount_factor * self.lambd) ** torch.arange(0, T-1, device=self.device)
         
         ep_ends = firsts
         ep_ends.append(T-1)
@@ -36,7 +60,6 @@ class PPOTrainer():
         for i in range(T-1):
             # discounts[i] = discounts[i:ep_ends[ep_idx]] * coeffs[:ep_ends[ep_idx]-i]
             discount = coeffs[:ep_ends[ep_idx] - i]
-            #print(i, discount.shape, deltas[i:ep_ends[ep_idx]])
             advantages.append(torch.sum(discount * deltas[i:ep_ends[ep_idx]]))
             if i+1 == ep_ends[ep_idx]:
                 deltas[i] -= self.discount_factor * values[i+1]
@@ -51,71 +74,146 @@ class PPOTrainer():
         advantages = []
         # for vals, rews, frsts in zip(values, rewards, firsts):
         #     advantages.append(self._compute_advantage_single_actor(vals, rews, frsts))
-        print(values.shape[0])
         for i in range(values.shape[0]):
             advantages.append(self._compute_advantage_single_actor(values[i], rewards[i], firsts[i]))
-        return advantages
+        advantages = torch.stack(advantages)
+        return (advantages - torch.mean(advantages)) / (torch.std(advantages) + 0.00001)
 
     def entropy(self, dist):
-        return -torch.sum(torch.log(dist) * dist)
+        return -torch.mean(torch.log(dist + 0.00000001) * dist)
+
+    def get_batches(self, batch_size, states, advantages, rewards, values, logits, acts):
+        num_batches = len(advantages) // batch_size
+        if len(advantages) // batch_size != 0:
+            num_batches += 1
+        indices = np.random.permutation(num_batches)
+        for i in indices:
+            yield (advantages[i * batch_size: min((i+1) * batch_size, len(advantages))],
+                    rewards[i * batch_size: min((i+1) * batch_size, len(rewards))],
+                    values[i * batch_size: min((i+1) * batch_size, len(values))],
+                    logits[i * batch_size: min((i+1) * batch_size, len(logits))],
+                    states[i * batch_size: min((i+1) * batch_size, len(states))],
+                    acts[i * batch_size: min((i+1) * batch_size, len(acts))])
 
     def train(self, env : ProcgenGym3Env):
+        self.pponetwork.train()
         obs_dims = list(env.ob_space["rgb"].shape)
         obs_dims[0] = obs_dims[-1]
         obs_dims[-1] = obs_dims[1]
         buf_dims = list(obs_dims)
         buf_dims.insert(0, self.num_actors)
-        buf_dims[1] *= 3
+        if self.concat_mode:
+            buf_dims[1] *= 3
+        best_reward = -10000
         for i in range(self.num_iters):
-            buffers = torch.zeros(buf_dims)
+            buffers = torch.zeros(buf_dims, device=self.device)
             firsts = [[] for _ in range(self.num_actors)]
             values = []
-            alt_values = torch.zeros(self.num_actors, self.num_timesteps)
+            alt_values = torch.zeros(self.num_actors, self.num_timesteps, device=self.device)
             rewards = []
-            alt_rewards = torch.zeros((self.num_actors, self.num_timesteps))
+            alt_rewards = torch.zeros((self.num_actors, self.num_timesteps), device=self.device)
             logitss = []
+            if self.concat_mode:
+                states = torch.zeros((self.num_actors * self.num_timesteps, 9, 64, 64), device=self.device)
+            else:
+                states = torch.zeros((self.num_actors * self.num_timesteps, 3, 64, 64), device=self.device)
+            alt_acts = torch.zeros((self.num_actors, self.num_timesteps), device=self.device, dtype=torch.long)
+            state_ctr = 0
             for t in range(self.num_timesteps):
                 rew, obs, first = env.observe()
-                rew = torch.tensor(rew)
+                rew = torch.tensor(rew, device=self.device, dtype=torch.float32)
+                # rew += 1.5
+                # rew /= 32.4
                 obs = obs["rgb"] / 255.0
-                obs = torch.tensor(obs)
+                obs = torch.tensor(obs, device=self.device, dtype=torch.float32)
                 obs = obs.permute(0, 3, 1, 2)
                 for n in range(self.num_actors):
-                    buffers = torch.roll(buffers, shifts = -3, dims=1)
-                    buffers[:, -3:, :, :] = obs
+                    if self.concat_mode:
+                        buffers = torch.roll(buffers, shifts = -3, dims=1)
+                        buffers[:, -3:, :, :] = obs
+                    else:
+                        buffers = obs
                     if first[n]:
                         firsts[n].append(t)
-                out = self.pponetwork(buffers)
-                value = out[:, 0]
-                logits = F.softmax(out[:, 1:], dim=-1)
-                #print("VALUE: {}, LOGITS: {}".format(value.shape, logits.shape))
-                logitss.append(logits)
+                states[state_ctr: state_ctr + len(buffers)] = buffers
+                with torch.no_grad():
+                    out = self.pponetwork(buffers)
+                    value = out[:, 0]
+                    logits = F.softmax(out[:, 1:], dim=-1)
+                logitss.append(logits.detach())
                 act = []
                 for j in range(self.num_actors):
-                    act.append(torch.multinomial(logits[j], num_samples=1).detach().numpy().squeeze())
-                values.append(value)
+                    dis = Categorical(logits[j])
+                    act_ = dis.sample().detach()
+                    act.append(act_.cpu().numpy().squeeze())
+                    alt_acts[j, t] = act_
+                values.append(value.detach())
                 alt_values[:, t] = value
+                
                 mapped_acts = [self.action_map[action] for action in act]
                 env.act(np.array(mapped_acts))
                 rewards.append(rew)
                 alt_rewards[:, t] = rew
+                state_ctr += len(buffers)
             # advantages = self._compute_advantages(values, rewards, firsts)
             advantages = self._compute_advantages(alt_values, alt_rewards, firsts)
             loss = 0
-            total_sz = 0
-            for t in range(self.num_timesteps-1):
-                for actor in range(self.num_actors):
-                    total_sz += 1
-                    clamped_rewards = rewards[t][actor].clamp(min=1-self.epsilon, max=1+self.epsilon)
-                    loss += torch.sum(torch.min(rewards[t][actor] * advantages[actor][t], clamped_rewards * advantages[actor][t]))
-                    loss -= self.c1 * torch.sum((values[t][actor] - rewards[t][actor]) ** 2)
-                    loss += self.c2 * self.entropy(logitss[t][actor])
-            loss /= total_sz
-            loss = -loss
-            avg_reward = sum(rewards) / self.num_actors
-            print("Iteration {} Training Loss {} Avg Reward {}".format(i, loss, avg_reward))
-            loss.backward()
-            self.optimizer.step()
+            avg_reward = alt_rewards.sum(dim=-1).mean()
+            advantages = torch.flatten(advantages, start_dim=0, end_dim=1).detach()
+            alt_rewards = torch.flatten(alt_rewards, start_dim=0, end_dim=1)[:-self.num_actors]
+            alt_values = torch.flatten(alt_values, start_dim=0, end_dim=1)[:-self.num_actors]
+            alt_acts = torch.flatten(alt_acts, start_dim=0, end_dim=1)[:-self.num_actors]
+            logitss = torch.flatten(torch.stack(logitss), start_dim=0, end_dim=1)[:-self.num_actors] # I hope that works
+            states = states[:-self.num_actors]
+            total_sz = len(advantages) * self.num_epochs
+            avg_loss = 0
+            #avg_reward = 0
+            avg_entropy = 0
+            avg_val_loss = 0
+            avg_surr_loss = 0
+            ev_avg = 0
+            for epoch in range(self.num_epochs):
+                data_gen = self.get_batches(self.batch_size, states, advantages, alt_rewards, alt_values, logitss, alt_acts)
+                for advantage, reward, value, logit, state, act in data_gen:
+                    out = self.pponetwork(state) # N x (1 + action_space)
+                    new_value = out[:, 0]
+                    new_logits = F.softmax(out[:, 1:], dim=-1)
+                    new_dist = Categorical(new_logits)
+                    old_dist = Categorical(logit)
+                    new_log_probs = new_dist.log_prob(act)
+                    old_log_probs = old_dist.log_prob(act)
+                    ratio = torch.exp(new_log_probs - old_log_probs)
+                    #ratio = torch.stack([ratio[m, act[m]] for m in range(len(ratio))])# ratio[torch.arange(len(ratio)), act]
+                    
+                    #advantage = advantage.view([-1, 1])
+                    clamped_reward = ratio.clamp(min=1-self.epsilon, max=1+self.epsilon)
+                    surr_loss = torch.mean(torch.min(ratio * advantage, clamped_reward * advantage))
+                    norm_reward = (reward + 1.5) / 32.4
+                    val_loss = torch.mean((new_value - norm_reward) ** 2)
+                    entr = self.entropy(new_logits)
+                    loss = -surr_loss + self.c1 * val_loss - (self.c2 * entr)
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+                    batch_sz = len(advantage)
+                    avg_loss += loss * batch_sz / total_sz
+                    ev = explained_variance(new_value, norm_reward)
+                    ev_avg += ev * batch_sz / total_sz
+                    #avg_reward += torch.sum(reward) / total_sz
+                    avg_entropy += entr / total_sz
+                    avg_val_loss += val_loss * batch_sz / total_sz
+                    avg_surr_loss += surr_loss * batch_sz / total_sz
+            print("Iteration {} Training Loss {:2f} Avg Reward {:2f} Avg Entropy {:2f} Avg Val_loss {:2f} Avg surr gain: {:2f} Avg exp variance: {:2f}".format(i, avg_loss, avg_reward, avg_entropy, avg_val_loss, avg_surr_loss, ev_avg))
+            
+            wandb.log({"Training loss": avg_loss,
+                     "Avg reward": avg_reward,
+                     "Avg entropy": avg_entropy,
+                     "Avg val loss": avg_val_loss,
+                     "Avg surr loss": avg_surr_loss,
+                     "Avg explained variance": ev_avg})
+            if avg_reward > best_reward:
+                best_reward = avg_reward
+                torch.save(self.pponetwork, "model.pt")
 
 
 